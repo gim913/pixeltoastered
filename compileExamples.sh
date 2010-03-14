9aa4820ef6511d9396e5b561fa2db9bf046c9521
GiM <gim913@gmail.com>
2010-03-14 12:53:10 +0100

    add alternative build script, some changes in basic build script

--- compileExamples.sh
+++ compileExamples.sh
@@ -3,7 +3,9 @@
 
 PIXEL_LOC=pixeltoaster-read-only
 PATH=/opt/dmd2/dmd/linux/bin:$PATH
-CXX_LINK_OPTS="-lstdc++ -lrt -lX11 -L/opt/dmd2/dmd/linux/lib -lphobos2"
+LINK=g++
+LINK_OPTS="-lstdc++ -lrt -lX11 -L/opt/dmd2/dmd/linux/lib -lphobos2"
+LINK_OUT="-o"
 
 g++ -O3 -DPLATFORM_UNIX -DPIXELTOASTER_TINY -c ${PIXEL_LOC}/PixelToaster.cpp -o PixelToasterCPP.o
 g++ -O3 -I${PIXEL_LOC} -c PixelToasterWrapper.cpp
@@ -11,23 +13,23 @@ g++ -O3 -I${PIXEL_LOC} -c PixelToasterWrapper.cpp
 dmd -release -c PixelToastereD.d
 
 dmd -release -c examples/FloatingPoint.d -ofexamples/FloatingPoint.o
-g++ -O3 examples/FloatingPoint.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/FloatingPoint 
+${LINK} ${LINK_OUT}examples/FloatingPoint examples/FloatingPoint.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS}
 
 dmd -release -c examples/Fullscreen.d -ofexamples/Fullscreen.o
-g++ -O3 examples/Fullscreen.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/Fullscreen
+${LINK} ${LINK_OUT}examples/Fullscreen examples/Fullscreen.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
 
 dmd -release -c examples/TrueColor.d -ofexamples/TrueColor.o
-g++ -O3 examples/TrueColor.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/TrueColor
+${LINK} ${LINK_OUT}examples/TrueColor examples/TrueColor.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
 
 dmd -release -c examples/Image.d -ofexamples/Image.o
-g++ -O3 examples/Image.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/Image
+${LINK} ${LINK_OUT}examples/Image examples/Image.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
 
 dmd -release -c examples/KeyaboardAndMouse.d -ofexamples/KeyaboardAndMouse.o
-g++ -O3 examples/KeyaboardAndMouse.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/KeyaboardAndMouse
+${LINK} ${LINK_OUT}examples/KeyaboardAndMouse examples/KeyaboardAndMouse.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
 
 dmd -release -version=UseDListener -c PixelToastereD.d
 dmd -release -version=UseDListener -c examples/KeyaboardAndMouseD.d -ofexamples/KeyaboardAndMouseD.o
-g++ -O3 examples/KeyaboardAndMouseD.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/KeyaboardAndMouseD
+${LINK} ${LINK_OUT}examples/KeyaboardAndMouseD examples/KeyaboardAndMouseD.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
 
 dmd -release -version=UseDListener -c examples/MultiDisplay.d -ofexamples/MultiDisplay.o
-g++ -O3 examples/MultiDisplay.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${CXX_LINK_OPTS} -o examples/MultiDisplay
+${LINK} ${LINK_OUT}examples/MultiDisplay examples/MultiDisplay.o PixelToastereD.o PixelToasterWrapper.o PixelToasterCPP.o ${LINK_OPTS} 
